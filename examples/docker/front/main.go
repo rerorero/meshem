fe69e763f9803d257da993d35cdfaac5d01575dd
rerorero <nato9598@hotmail.co.jp>
2018-03-22 08:58:14 +0900

    implemented zipkin propagation

--- examples/docker/front/main.go
+++ examples/docker/front/main.go
@@ -8,9 +8,39 @@ import (
 	"os"
 )
 
+var (
+	client       = http.Client{}
+	traceHeaders = []string{
+		"X-Ot-Span-Context",
+		"X-Request-Id",
+		"X-B3-TraceId",
+		"X-B3-SpanId",
+		"X-B3-ParentSpanId",
+		"X-B3-Sampled",
+		"X-B3-Flags",
+	}
+)
+
+func propagate(in *http.Request, out *http.Request) {
+	for _, key := range traceHeaders {
+		value := in.Header.Get(key)
+		if value != "" {
+			out.Header.Add(key, value)
+		}
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	appHost := os.Getenv("egress_app1")
-	resp, err := http.Get(appHost)
+	req, err := http.NewRequest("GET", appHost, nil)
+	if err != nil {
+		fmt.Fprintf(w, "Error!!! %s\n", err.Error())
+	}
+
+	// copy headers for tracing
+	propagate(r, req)
+
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Fprintf(w, "Error!!! %s\n", err.Error())
 	}
@@ -22,7 +52,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func newLogHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		log.Printf("%s %s %s header=%+v", r.RemoteAddr, r.Method, r.URL, r.Header)
 		handler.ServeHTTP(w, r)
 	})
 }
